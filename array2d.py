2ee30fc05f5bc013b65507875d1a25468bf45df9
Ganesh Babu <readingroomofgb@gmail.com>
2019-10-06 22:51:14 +0530

    Cleaned up comments and unused variables

--- array2d.py
+++ array2d.py
@@ -5,43 +5,37 @@ H=[
     [9,10,33,12],
     [13,14,15,16]
     ]
-#i=1;j=1
-Z=[]
-#print(a[i-1][j-1],a[i-1][j],a[i-1][j+1]) ; print(" ",a[i][j]);print(a[i+1][j-1],a[i+1][j],a[i+1][j+1])
 
-def hourglass(a,i,j):
+
+
+def hourglass(a,i,j): # The hourglass function
     
     sum = int(0)
-    #print( a[i-1][j-1],a[i-1][j],a[i-1][j+1]) ; print(" ",a[i][j]);print(a[i+1][j-1],a[i+1][j],a[i+1][j+1] )
-    for p in a[i-1][j-1],a[i-1][j],a[i-1][j+1],a[i][j],a[i+1][j-1],a[i+1][j],a[i+1][j+1]:
+    
+    for p in a[i-1][j-1],a[i-1][j],a[i-1][j+1],a[i][j],a[i+1][j-1],a[i+1][j],a[i+1][j+1]:  # iterating over the hourglass 
         sum += p
 
-    #print(sum)
     return sum
 
-'''c=hourglass(H,1,1)
-print("Sum is:",c)
-print(); */
-#hourglass(H,2,2)'''
 
 s = [] # Empty single dimensional list
-#e = [ [ 0 for r in range(0,2)] for col in range(0,2)]
-#print(e[0][0])
+
 for i in range(1,3):
     for j in range(1,3):
         d = hourglass(H,i,j)
-       # e[i-1][j-1] = int(d)
-        #print(d)
+     
         s.append(int(d))
 
-print(s)
 
+
+#Bubble sorting the array with sum of hourglasses in descending order 
 n = len(s)
 for i in range(n):
     for j in range(n-i-1):
         if s[j] < s[j+1]:
             s[j],s[j+1] = s[j+1],s[j]
 
+#Printing the maximum of sum
 print(s[0])
         
 
